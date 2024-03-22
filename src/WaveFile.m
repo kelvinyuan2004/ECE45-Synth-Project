29111d60526d2975a5a99448695b9dd11b250ccc
ky <kelvinyuan2004@gmail.com>
2024-03-22 15:02:53 -0700

    refactored variables, begin amplitude scale function

--- src/WaveFile.m
+++ src/WaveFile.m
@@ -8,10 +8,15 @@ WaveFile class.
 
 instance vars:
 filePath - path of file
+audioData - array storing amplitudes
+fs - sampling rate
 %}
 classdef WaveFile
     properties
         filePath
+        timeData
+        fs
+        freqData
     end
     
     methods
@@ -22,21 +27,40 @@ classdef WaveFile
         @param filePath - desired location of our wav file
         %}
         function obj = WaveFile(filePath)
+
             obj.filePath = filePath;
+            [obj.timeData, obj.fs] = audioread(obj.filePath);
+            obj.freqData = fft(obj.timeData);
         end
         
         %{
         Method to play the current audio
         %}
         function play(obj)
+
             clear sound;
             % play audio
-            [audioData, fs] = audioread(obj.filePath);
-            sound(audioData, fs); % Adjust sampling rate as needed
+            [obj.timeData, obj.fs] = audioread(obj.filePath);
+            sound(obj.timeData, obj.fs); % Adjust sampling rate as needed
+        end
+
+        %{
+        Method to play the current audio
+        %}
+        function obj = changeAmplitude(obj, amplitudeScale)
+
+            %ref: https://www.mathworks.com/matlabcentral/answers/348355-how-do-i-limit-the-values-in-an-array
+
+            obj.freqData = obj.freqData*amplitudeScale;
+
+            % Make sure absolute value of amplitudes is max 1 to comply
+            % with wav file standards.
+            obj.freqData = max(min(obj.freqData, 1), -1);
         end
     end
     
     methods(Static)
+        
         %{
         Method to stop playing audio
         %}
