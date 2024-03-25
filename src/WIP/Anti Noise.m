233b9aa53d613ff9a1afd0cb7acf8412627b7941
phanc2069 <58751173+phanc2069@users.noreply.github.com>
2024-03-24 22:32:55 -0700

    Update Anti Noise.m
    Final version

--- src/WIP/Anti Noise.m
+++ src/WIP/Anti Noise.m
@@ -3,7 +3,7 @@ t = [0:1/fs:size];
 mu = .006;
 N = fs*2;
 
-input = (tie to device microphone or smth)
+%input = (tie to device microphone or smth)
 noise = cos(2*pi*99/2*t);
 
 primary = input + noise;
@@ -12,6 +12,14 @@ primary = input + noise;
 
 for i=1:N-order
 	buffer = ref(i:i+order-1);
-	desired(i) = primary(i)-buffer*x
+	desired(i) = primary(i)-buffer*x;
 	x=x+(buffer.*mu*desired(i)/norm(buffer));
 end
+
+%*********************
+%The idea:
+% 1. Take an input provided by the user
+% 2. Add noise to the signal
+% 3. Clear the signal with the function
+% 4. Playback or show the "de-noised" sound.
+%*********************
